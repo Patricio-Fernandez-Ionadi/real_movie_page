b5b7821e8770978ecddab1c1b1a19a8f5789f0db
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2021-12-16 15:22:16 -0300

    caratula de tvShows y presentacion de header

--- front/src/components/TvDetailComponent.jsx
+++ front/src/components/TvDetailComponent.jsx
@@ -2,11 +2,12 @@ import React from "react"
 import { environment } from "../privateKeys"
 import { makeStyles } from "@material-ui/core"
 
-const useStyles = makeStyles(({ theme }) => ({
+const useStyles = makeStyles(() => ({
 	container: {
+		color: "#fff",
 		width: "100%",
-		minHeight: "80vh",
-		maxHeight: "80bh",
+		minHeight: "50vh",
+		maxHeight: "70vh",
 		backgroundImage: ({ backdrop_path }) =>
 			`url('${environment.IMAGE_URI}${backdrop_path}')`,
 		backgroundSize: "cover",
@@ -32,28 +33,86 @@ const useStyles = makeStyles(({ theme }) => ({
 		justifyContent: "center",
 		alignItems: "center",
 	},
+	imgContainer: {
+		width: "70%",
+		display: "flex",
+		justifyContent: "center",
+	},
+	poster: {
+		width: "100%",
+		maxWidth: "250px",
+	},
+
 	overview: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
 		zIndex: 2,
 		width: "70%",
+		// background: "red",
+	},
+	overviewTitle: {
+		width: "100%",
+		// background: "yellow",
+	},
+	overviewCalification: {
+		// background: "red",
+		padding: "10px",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: " center",
 	},
 }))
 
 const TvDetailComponent = ({ tvshow }) => {
 	const classes = useStyles(tvshow)
+	console.log(tvshow)
 	return (
 		<div className={classes.container}>
 			<div className={classes.sombreado}></div>
+
 			<div className={classes.portada}>
-				<img
-					style={{ maxWidth: "70%" }}
-					src={`${environment.IMAGE_URI}${tvshow.poster_path}`}
-					alt={`${tvshow.first_air_date ? "Tv Show" : "tvshow"} poster`}
-				/>
+				<div className={classes.imgContainer}>
+					<img
+						className={classes.poster}
+						src={`${environment.IMAGE_URI}${tvshow.poster_path}`}
+						alt={`${tvshow.first_air_date ? "Tv Show" : "tvshow"} poster`}
+					/>
+				</div>
 			</div>
-			<div>
-				<p>
-					{tvshow.original_name} - {tvshow.id}
-				</p>
+
+			<div className={classes.overview}>
+				<h1 className={classes.overviewTitle}>
+					{tvshow.name} | {tvshow.tagline}
+				</h1>
+				<small>
+					seasons: {tvshow.seasons.length} |{" "}
+					{tvshow.first_air_date.split("-")[0]}
+				</small>
+
+				<div>
+					<h2>overview</h2>
+					<p>{tvshow.overview}</p>
+
+					<div className={classes.overviewCalification}>
+						<span>
+							popularity:{" "}
+							{Math.ceil(tvshow.popularity / tvshow.vote_average) / 10}% |{" "}
+							calification: {tvshow.vote_average} | votes: {tvshow.vote_count}
+						</span>
+					</div>
+
+					<div>
+						<p>Titulo original: {tvshow.original_name}</p>
+						<p>
+							Creador:{" "}
+							{tvshow.created_by.map((each) => (
+								<span> {each.name} </span>
+							))}
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
